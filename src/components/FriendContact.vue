94ef4d18a9eab7d435af02c6c27cfb92ac71a9a2
effycoco <734157786@qq.com>
2022-04-30 00:27:42 +0800

    redo with vue-cli

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <h2>{{ friendInfo.name }}</h2>
+    <button @click="toggleDetails">
+      {{ isDetailsShowed ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="isDetailsShowed">
+      <li><strong>Phone:</strong> {{ friendInfo.phone }}</li>
+      <li><strong>Email:</strong> {{ friendInfo.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  props: {
+    friendInfo: Object,
+  },
+  data() {
+    return {
+      isDetailsShowed: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.isDetailsShowed = !this.isDetailsShowed;
+    },
+  },
+};
+</script>
